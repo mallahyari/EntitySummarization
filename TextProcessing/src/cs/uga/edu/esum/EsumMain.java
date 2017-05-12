d73ae059e2a1e5bca5cc20a23ccdb942c75f6eef
Mehdi <Mehdi@s172-20-121-h219.paws.uga.edu>
2017-05-12 14:56:37 -0400

    hi

--- TextProcessing/src/cs/uga/edu/esum/EsumMain.java
+++ TextProcessing/src/cs/uga/edu/esum/EsumMain.java
@@ -19,53 +19,29 @@ public class EsumMain {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException { 
-	
-		/* Step 1: if necessary
-		 * readWriteEntity : Read entity file as input (instances.txt) write entity to name only file as output (entNameOnly.txt)
-		 */
-		//EntityProc readWriteEnt =new EntityProc();
-		//readWriteEnt.readWriteEntity();
-		//**********************\\\
-	
+//		EntityProc readWriteEnt =new EntityProc();
+//		readWriteEnt.readWriteEntity();
 		
-		/* Step 2: 
-		 * Read entity Name only (entNameOnly.txt) as input and extract all useful predicates
-		 * predicateChecker will also Call predicateExtractor to extract all predicates and Objects for all entities in entNameOnly.txt
-		 * 
-		 * predicateExtractor taking entity name and extract all predicates (meaningful ones) . it makes doc from all extracted Objacts (Barack_Obama.txt)
-		 * Also predicateExtractor makes docToID.txt contains all entities with ID (Barack_Obama  0)
-		 * It also makes creating a MAP from each Word to ID (mapWordToID)
-		 *
-		 *making PredicateList 	//Making Predicate list text file predicateList.txt  contains full URL predicate with a unique number. This functions needs 
-		 *to be ran after predicateChekcer because this function will fill out the list of perdicate in predicateSet 
-		 */
-		
-	//	EntityProc predicCheck =new EntityProc();
-	//	predicCheck.predicateChecker();
-	//	predicCheck.makingPredicateList();
 	
-		/*
-		 * Step 3 :  Extract Domain and Range for each predicate and make predicateDomainRange.txt
-		 */
-		//	predicCheck.domainRangeExtractor();
+		EntityProc predicCheck =new EntityProc();
+		predicCheck.predicateChecker();
+		predicCheck.makingPredicateList();
+		predicCheck.domainRangeExtractor();
 		
-		/*
-		 * Step 4 :  create corpus.txt (entityID, wordID, frequency)
-		 */	
-		//EntityProc corpusMaker =new EntityProc();
-		//corpusMaker.corpusMaker();
+		
+//		EntityProc corpusMaker =new EntityProc();
+//		corpusMaker.corpusMaker();
 		
 //	SumModelParameters paramIniti =new SumModelParameters();
 		
 //******************************************************\\
 		
-
-		EntSum entSum = new EntSum();
-		entSum.initializeMatrices();
+		
+//		EntSum entSum = new EntSum();
+//		entSum.initializeMatrices();
 		
 		// Gibbs Sampling Initialization and Run //
-		entSum.initializeGibbsSampling();
-		
+//		entSum.initializeGibbsSampling();
 //		entSum.run();
 		
 
