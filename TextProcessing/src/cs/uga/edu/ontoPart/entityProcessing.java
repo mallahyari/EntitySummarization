9bda516f3a47598ca457b0ba1ca6fea39fe704c5
amin <amin@192.168.1.3>
2018-04-29 17:44:47 -0400

    m

--- TextProcessing/src/cs/uga/edu/ontoPart/entityProcessing.java
+++ TextProcessing/src/cs/uga/edu/ontoPart/entityProcessing.java
@@ -149,7 +149,7 @@ public class entityProcessing {
 			//Connecting to Virtuoso to extract predicates and objects
 			StringBuffer queryString = new StringBuffer();
 			queryString.append("SELECT ?s FROM <" + GRAPH + "> WHERE { ");
-			queryString.append(" ?s a <" + uriClassPrefix + className + "> FILTER ( 1 >  <SHORT_OR_LONG::bif:rnd> (10, ?s))  " );
+			queryString.append(" ?s a <" + uriClassPrefix + className + ">  FILTER ( 1 >  <SHORT_OR_LONG::bif:rnd> (10, ?s)) " );
 			queryString.append("} ORDER BY RAND()  Limit 200");
 			System.out.println(queryString);
 			Query sparql = QueryFactory.create(queryString.toString());
