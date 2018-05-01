d86208147d3c9aec46a7b42380ed1742b38b47d8
amin <amin@192.168.1.3>
2018-04-30 21:21:02 -0400

    g

--- TextProcessing/src/cs/uga/edu/ontoPart/entityProcessing.java
+++ TextProcessing/src/cs/uga/edu/ontoPart/entityProcessing.java
@@ -318,7 +318,7 @@ public class entityProcessing {
 							//Store pair of predicate*object with ID
 							if (predicateObjectIdMap.get(predicateName+"*"+objectName) == null) {
 								predicateObjectIdMap.put(predicateName+"*"+objectName, prediateObjectIdGenerator);
-								predicateObjectPair.write(objectName + " " + prediateObjectIdGenerator + "\n");
+								predicateObjectPair.write(predicateName+"*"+objectName + " " + prediateObjectIdGenerator + "\n");
 								prediateObjectIdGenerator++;
 							}
 							
