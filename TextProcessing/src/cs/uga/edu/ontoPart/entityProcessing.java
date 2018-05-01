d92f01adaee3ec69251d077759316b4ade7a141c
amin <amin@192.168.1.3>
2018-05-01 14:18:22 -0400

    v

--- TextProcessing/src/cs/uga/edu/ontoPart/entityProcessing.java
+++ TextProcessing/src/cs/uga/edu/ontoPart/entityProcessing.java
@@ -193,7 +193,7 @@ public class entityProcessing {
 			StringBuffer queryString = new StringBuffer();
 			queryString.append("SELECT ?s FROM <" + GRAPH + "> WHERE { ");
 			queryString.append(" ?s a <" + uriClassPrefix + className + ">   " );
-			queryString.append("}   Limit 2");
+			queryString.append("}   Limit 3");
 			System.out.println(queryString);
 			Query sparql = QueryFactory.create(queryString.toString());
 			VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create (sparql, virtGraph);
