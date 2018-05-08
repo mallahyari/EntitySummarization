58f89fc3f4e77df2f5307315d635ac53817a66a3
amin <amin@192.168.1.3>
2018-05-08 19:09:50 -0400

    f

--- TextProcessing/src/cs/uga/edu/simpleOntoPart/simpleOntoParameters.java
+++ TextProcessing/src/cs/uga/edu/simpleOntoPart/simpleOntoParameters.java
@@ -20,6 +20,8 @@ import java.util.Set;
 /**
  * @author mehdi
  *
+ * updated for ontoPart
+ *
  */
 public class simpleOntoParameters {
 	
@@ -29,24 +31,28 @@ public class simpleOntoParameters {
 	public int [] docIds = null;
 	public int [] wordIds = null;
 	public int [] wordsCounts = null;
-	public int D = 0;
-	public int W = 0;
-//	public int T = 22;
+	public int D = 0; //numberOfEntity /Documents
+	public int W = 0; //sizeOfVocabulary(Object)
+	public int T = 0; //number of topics
 	public int T1 = 57;
 	public int T2 = 133;
 	public int P = 0; 
-	public int N = 0;
+	public int C = 0;//Number of all classes 
+	public int N = 0; //numberOfAllObjects(Words)
 	public int nIterations = 2000;
 	int burnIn = 200;
 	public double ALPHA = 60.0 / 390;
 	public final double BETA  = 0.01;
+	public final double ZETA  = 0.01;
 //	public final double TAU = 50.0 / T;
 //	public final double EPSILON = 0.9;
 	public final double gamma = 0.01	;
 	public String corpusFilename = "/home/mehdi/simpleOntoPart/evaluation/corpus.txt";
 	public String entitiesFilename = "/home/mehdi/simpleOntoPart/evaluation/entNameOnly.txt";
 	public String corpusStatFilename = "/home/mehdi/simpleOntoPart/evaluation/corpusStatistics.txt";
-	public String predicateFilename = "/home/mehdi/simpleOntoPart/evaluation/predicateToId.txt";
+//	public String predicateFilename = "/home/mehdi/ontoPart/evaluation/predicateToId.txt";
+	public String classFilename = "/home/mehdi/simpleOntoPart/evaluation/classListandID.txt";
+
 //	public String corpusFilename = "/home/mehdi/taxonomyProject/preprocessedFiles/corpus.txt";
 //	public String entitiesFilename = "/home/mehdi/taxonomyProject/preprocessedFiles/corpusConceptsSr.txt";
 //	public String corpusStatFilename = "/home/mehdi/taxonomyProject/preprocessedFiles/corpusStatistics.txt";
@@ -65,10 +71,11 @@ public class simpleOntoParameters {
 		wordsCounts = new int [corpus.size()];
 		Set<Integer> uniqueWordIds = new HashSet<Integer>();
 		Set<Integer> uniqueDocIds = new HashSet<Integer>();
-//		Set<Integer> td = new HashSet<Integer>();
-//		for (int i = 0; i < 7104; i++) {
-//			td.add(i);
-//		}
+		Set<Integer> classIds = new HashSet<Integer>(); //Set of classIds
+		
+		
+		System.out.println("corpos size:"+corpus.size());
+		
 		for (int i = 0;i < corpus.size();i++) {
 			String [] tokens = corpus.get(i).split(" ");
 			docIds [i] = Integer.parseInt(tokens [0]);
@@ -81,39 +88,25 @@ public class simpleOntoParameters {
 //		for (int i: td){
 //			if (!uniqueDocIds.contains(i)) System.out.println(i);
 //		}
+
 		D = uniqueDocIds.size() ;
 		W = uniqueWordIds.size();
-
-		BufferedWriter bw1 = null;
-		try {
-
-			bw1 = new BufferedWriter(new FileWriter(corpusStatFilename, true));
-			bw1.write(  "numberOfEntity, sizeOfVocabulary(Object), numberOfAllObjects(Words)");
-			bw1.newLine();
-			bw1.write(  D + " " + W + " " + N);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			bw1.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("D: " + D + " W: " + W + " N: " + N);
-
-		//Counting number of Predicate or P 
+		System.out.println("D:"+D +"         W:"+W);
+		
+		//Counting number of classes C, reading text file and extract number of classes. 
 
 		BufferedReader reader=null;
 		try {
-			reader = new BufferedReader(new FileReader(predicateFilename));
+			reader = new BufferedReader(new FileReader(classFilename));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
 		try {
-			while (reader.readLine() != null) P++;
+			while (reader.readLine() != null) {
+				classIds.add(C); //Filling classIds Set. 
+				C++;
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -122,7 +115,28 @@ public class simpleOntoParameters {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Number of Predicate =" + P);
+		System.out.println("Number of Classes =" + C);
+		
+		
+		
+		
+		BufferedWriter bw1 = null;
+		try {
+			bw1 = new BufferedWriter(new FileWriter(corpusStatFilename, true));
+			bw1.write(  "numberOfEntity, sizeOfVocabulary(Object), numberOfAllObjects(Words), number of classes");
+			bw1.newLine();
+			bw1.write(  D + " " + W + " " + N + " " + C);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			bw1.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("D: " + D + " W: " + W + " N: " + N + " C: " + C);
+
+		
 	} // end of initializeParameters
 	
 	public void fillArrays() {
@@ -143,6 +157,9 @@ public class simpleOntoParameters {
 //		double dt = (System.currentTimeMillis() - st);// / 1000.;
 //		System.out.println("time: " + dt);
 		System.out.println("done!");
+		System.out.println("doc!" + d.length);
+		System.out.println("w!" + w.length);
+		
 	} // end of fillArrays
 	
 	public List<String> readFile(String filename) {
@@ -151,6 +168,7 @@ public class simpleOntoParameters {
 			BufferedReader br = new BufferedReader(new FileReader(filename));
 			String line = "";
 			while ((line = br.readLine()) != null) {
+				System.out.println(line);
 				corpus.add(line);
 			} // end of while
 			br.close();
