d35f6bb54d54d4172a4d1a58f542d1a4c1d6f4a0
amin <amin@192.168.1.3>
2018-05-13 18:10:23 -0400

    mn

--- TextProcessing/src/cs/uga/edu/simpleOntoPart/entityProcessing.java
+++ TextProcessing/src/cs/uga/edu/simpleOntoPart/entityProcessing.java
@@ -620,9 +620,9 @@ public void createPredicateObjectPairClassMatrix() throws IOException{
     			System.out.println(i+"********** Predicate"+ myPredicate[0]);
     			
     			
-    			//for (int j = 0; j < numOfClass; j++) {
-    			//	subjectNames.addAll(subjectNameSet);
-    				
+    			for (int j = 0; j < numOfClass; j++) {
+    				subjectNames.addAll(subjectNameSet);
+    				System.out.println(j+"********** classsssss"+ classNameToIdMap1.get(j));
     			//	instanceSet=getInstances(myPredicate[0],classNameToIdMap1.get(j));
     			//	System.out.println(i +"     "+ j+ "    "+instanceSet.size() + "          SubjectName Size:"+ subjectNames.size() + "          SubjectNameSeTTTT Size:"+ subjectNameSet.size());
     				//subjectNames.retainAll(instanceSet);
@@ -636,7 +636,7 @@ public void createPredicateObjectPairClassMatrix() throws IOException{
 //    					predicateObjectClassWeight[i][j]=1;
 //    				}
 //    				
-    			//} // end of for (j)
+    			} // end of for (j)
     		} // end of for (i)
             
            
