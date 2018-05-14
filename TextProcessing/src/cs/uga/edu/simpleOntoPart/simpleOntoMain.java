1265a5cd85f60b1231c8d78fddc71802e7c0f2ea
amin <amin@192.168.1.3>
2018-05-14 14:37:52 -0400

    d

--- TextProcessing/src/cs/uga/edu/simpleOntoPart/simpleOntoMain.java
+++ TextProcessing/src/cs/uga/edu/simpleOntoPart/simpleOntoMain.java
@@ -99,9 +99,9 @@ public class simpleOntoMain {
 		entityProcessing createMyEntityList = new entityProcessing();
 	//	createMyEntityList.createEntityList();
 	    createMyEntityList.corpusMaker();
-		simpleOntoParameters myinitialization=new simpleOntoParameters();
+		//simpleOntoParameters myinitialization=new simpleOntoParameters();
 		
-		createMyEntityList.createPredicateObjectPairTopicMatrix(158234,10);
+		//createMyEntityList.createPredicateObjectPairTopicMatrix(158234,10);
 		
 	    
 		
