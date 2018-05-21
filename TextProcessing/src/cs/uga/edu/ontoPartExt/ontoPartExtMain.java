deea9d168c732faae8b40a6d382fb73b75d677c8
amin <amin@192.168.1.3>
2018-05-21 14:18:20 -0400

    csv

--- TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
+++ TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
@@ -110,15 +110,15 @@ public class ontoPartExtMain {
 //		ontoPartExtParameters myinitialization=new ontoPartExtParameters();
 		
 		//Matrix size : Topic X Vocab size 20X 158234
-		createMyEntityList.createPredicateObjectPairTopicMatrix(20,186388);
-//		
+//		createMyEntityList.createPredicateObjectPairTopicMatrix(20,186388);
+////		
 		ontoPartExtModel myOnto = new ontoPartExtModel();
-		myOnto.initializeMatrices();
-		// Gibbs Sampling Initialization and Run //
-		myOnto.initializeGibbsSampling();
-		myOnto.run();
+//		myOnto.initializeMatrices();
+//		// Gibbs Sampling Initialization and Run //
+//		myOnto.initializeGibbsSampling();
+//		myOnto.run();
 		
-//		myOnto.writeToCSV();
+		myOnto.writeToCSV();
 	}
 
 }
