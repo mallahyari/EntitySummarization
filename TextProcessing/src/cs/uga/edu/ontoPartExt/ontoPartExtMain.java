2ed7e7ab0c00b554610a5f734577423df66e5398
amin <amin@192.168.1.3>
2018-05-21 22:23:59 -0400

    cvc

--- TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
+++ TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
@@ -113,12 +113,12 @@ public class ontoPartExtMain {
 //		createMyEntityList.createPredicateObjectPairTopicMatrix(20,136338);
 ////		
 		ontoPartExtModel myOnto = new ontoPartExtModel();
-		myOnto.initializeMatrices();
-		// Gibbs Sampling Initialization and Run //
-		myOnto.initializeGibbsSampling();
-		myOnto.run();
+//		myOnto.initializeMatrices();
+//		// Gibbs Sampling Initialization and Run //
+//		myOnto.initializeGibbsSampling();
+//		myOnto.run();
 		
-//		myOnto.writeToCSV();
+		myOnto.writeToCSV();
 	}
 
 }
