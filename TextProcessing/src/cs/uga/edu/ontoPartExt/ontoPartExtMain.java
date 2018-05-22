42a78ee889a71d9058bc7a12e5f4be41313c2ed1
amin <amin@192.168.1.3>
2018-05-21 22:16:36 -0400

    nbc

--- TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
+++ TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
@@ -102,23 +102,23 @@ public class ontoPartExtMain {
 		//		2) Line  113
 		//		3) line 116-118-119
 		//		4) line 121
-//		entityProcessingExt createMyEntityList = new entityProcessingExt();
-//		createMyEntityList.createEntityList();
-//		System.out.println("MyEntityList Done!"); 
-//	    createMyEntityList.corpusMaker();
-//	    System.out.println("corpusMaker Done!"); 
-//		ontoPartExtParameters myinitialization=new ontoPartExtParameters();
+		entityProcessingExt createMyEntityList = new entityProcessingExt();
+		createMyEntityList.createEntityList();
+		System.out.println("MyEntityList Done!"); 
+	    createMyEntityList.corpusMaker();
+	    System.out.println("corpusMaker Done!"); 
+		ontoPartExtParameters myinitialization=new ontoPartExtParameters();
 		
 		//Matrix size : Topic X Vocab size 20X 158234 186387
 //		createMyEntityList.createPredicateObjectPairTopicMatrix(20,163036);
 ////		
-		ontoPartExtModel myOnto = new ontoPartExtModel();
+//		ontoPartExtModel myOnto = new ontoPartExtModel();
 //		myOnto.initializeMatrices();
 //		// Gibbs Sampling Initialization and Run //
 //		myOnto.initializeGibbsSampling();
 //		myOnto.run();
 		
-		myOnto.writeToCSV();
+//		myOnto.writeToCSV();
 	}
 
 }
