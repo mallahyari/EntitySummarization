97f4a3428d27a956d140e00cded368e9891bb391
amin <amin@192.168.1.6>
2018-06-14 14:25:07 -0400

    p1

--- TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
+++ TextProcessing/src/cs/uga/edu/ontoPartExt/ontoPartExtMain.java
@@ -103,14 +103,14 @@ public class ontoPartExtMain {
 		//		3) line 116-118-119
 		//		4) line 121
 		entityProcessingExt createMyEntityList = new entityProcessingExt();
-		createMyEntityList.createEntityList();
-		System.out.println("MyEntityList Done!"); 
-	    createMyEntityList.corpusMaker();
-	    System.out.println("corpusMaker Done!"); 
-		ontoPartExtParameters myinitialization=new ontoPartExtParameters();
+//		createMyEntityList.createEntityList();
+//		System.out.println("MyEntityList Done!"); 
+//	    createMyEntityList.corpusMaker();
+//	    System.out.println("corpusMaker Done!"); 
+//		ontoPartExtParameters myinitialization=new ontoPartExtParameters();
 		
 		//Matrix size : Topic X Vocab size 20X 158234 186387 166167
-//		createMyEntityList.createPredicateObjectPairTopicMatrix(10,154050);
+		createMyEntityList.createPredicateObjectPairTopicMatrix(10,154050);
 ////		
 //		ontoPartExtModel myOnto = new ontoPartExtModel();
 //		myOnto.initializeMatrices();
